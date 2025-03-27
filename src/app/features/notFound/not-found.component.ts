afb30069443b4730c3c57ad60958e1b6525b5345
nero-gh <bismark.kadogbe@amalitech.com>
2025-03-27 11:25:43 +0000

    feat: add new components and services for admin and not-found pages
    This commit introduces new components, services, and directives to support the admin dashboard and not-found page functionality. The changes include the creation of the auth service, dashboard routes, and various components such as the dashboard layout and not-found page. These additions are essential for enhancing the application's navigation and user experience.

---
+++ src/app/features/notFound/not-found.component.ts
@@ -0,0 +1,15 @@
+import {Component} from "@angular/core";
+
+
+
+@Component({
+  selector:"not-found",
+  standalone:true,
+  templateUrl:"./not-found.component.html",
+  styleUrls:["./not-found-component.scss"]
+})
+
+export class NotFoundComponent{
+  constructor() {
+  }
+}
